a7c79b59c30f8ab3d419764632147a3a28980f63
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2024-12-30 15:43:29 +0300

    Create loading screen, add zustand, update prettier and linter

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "@pages/index.tsx";
+import { LoadingScreen } from "@features/preloading-screen";
 
 import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <LoadingScreen />
     <App />
   </StrictMode>,
 );
