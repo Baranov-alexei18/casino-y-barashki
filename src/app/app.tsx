760db854c87b2e4e0353a97fb90452110ba7fa89
Alexey Baranov <65853601+Baranov-alexei18@users.noreply.github.com>
2024-12-31 09:35:27 +0300

    Create Header

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "@pages/index.tsx";
+import { App } from "@pages/index.tsx";
 import { LoadingScreen } from "@features/preloading-screen";
 
 import "./global.css";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,11 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 
-import styles from "./styles.module.css";
-import viteLogo from "../assets/vite.svg";
+import { Header } from "@shared/components/Header";
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <Fragment>
-      <div className={styles.wrapper}>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={styles.card}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className={styles.readTheDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
     </Fragment>
   );
 };
-
-export default App;
